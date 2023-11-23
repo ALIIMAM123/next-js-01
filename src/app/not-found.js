d4d1d740f1a96772a29d90964e2ba9c4f6ce6c81
Md Ali imam <imam.ali1751@gmail.com>
2023-11-23 20:57:59 +0530

    global-404-page-and-page-specific-404-page

---
+++ src/app/not-found.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const GlobalNotFoundPage = () => {
+    return (
+        <div>
+            <h1>This is Global Not found Page</h1>
+        </div>
+    )
+}
+
+export default GlobalNotFoundPage
